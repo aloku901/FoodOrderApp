1b3dd286ab4949df8e2d52e9b1e0b57f30e60505
Alok Upadhyay <aloku901@gmail.com>
2023-11-06 17:48:47 +0530

    Add Top Rated Restaurant Features

---
+++ src/components/RestaurantCard.jsx
@@ -0,0 +1,34 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = ({resData}) => {
+   
+  
+    const {
+      cloudinaryImageId,
+      name,
+      avgRating,
+      cuisines,
+      costForTwo,
+      deliveryTime,
+    } = resData?.data;
+  
+    return (
+      <div className="res-card">
+        <img
+          className="res-logo"
+          alt="res-logo"
+          src={
+            CDN_URL +
+            cloudinaryImageId
+          }
+        />
+        <h3>{name}</h3>
+        <p>{cuisines.join(", ")}</p>
+        <h4>{avgRating} ★  .{deliveryTime} min</h4>
+        <h4>₹{costForTwo / 100} FOR TWO</h4>
+        <h4></h4>
+      </div>
+    );
+};
+
+export default RestaurantCard;
